63be377df382585a456447c16367d6e58803d81c
Alpesh-Jadav <alpeshjadav11899@gmail.com>
2020-08-24 07:07:35 -0700

    sidebar options styles commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 import './Sidebar.css'
 import SidebarOption from './SidebarOption'
 function Sidebar() {
@@ -6,11 +8,15 @@ function Sidebar() {
         <div className="sidebar">
         <img className="sidebar__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify"/>
         
-        <SidebarOption title="Home"/>
-        <SidebarOption title="Search"/>
-        <SidebarOption title="Your Library"/>
+        <SidebarOption title="Home" Icon={HomeOutlinedIcon}/>
+        <SidebarOption title="Search" Icon={SearchIcon}/>
+        <SidebarOption title="Your Library" Icon={LibraryMusicIcon}/>
+         <br/>
+         <strong className="sidebar__title">PLAYLISTS</strong>
+         <hr />
         </div>
     )
 }
+
 
 export default Sidebar
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import './SidebarOption'
+import './SidebarOption.css'
 function SidebarOption({ title, Icon}) {
     return (
         <div className="sidebarOption">
-            <p>{title}</p>
+           {Icon && <Icon className="sidebarOption__icon" />}
+            {Icon ? <h4>{title}</h4> : <p>{title}</p> }
         </div>
     )
 }
 
 export default SidebarOption
+
