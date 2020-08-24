d0ee094ec65190d78e3f58b523f5f90efdbfa709
Alpesh-Jadav <alpeshjadav11899@gmail.com>
2020-08-24 05:16:25 -0700

    Context api commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
 
+const spotify = new SpotifyWebApi();
 function App() {
+  const [token, setToken] = useState(null)
 
+  const [{}, dispatch] = useDataLayerValue();
   useEffect(() => {
-    const token = getTokenFromUrl();
+    const hash = getTokenFromUrl();
+    window.location.hash="";
+
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token)
+      spotify.setAccessToken(_token)
+
+      spotify.getMe().then(user => {
+        console.log(user);
+      })
+    }
     console.log('token', token)
-  }, [])
+  }, []) 
 
   return (
     <div className="app">
-
-     <Login />
+      {
+        token ? (
+         
+          <Player />
+        ) : (
+          <Login />
+        )
+      }
+     
 
     </div>
   );
