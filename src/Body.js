1c72e04721946504f0c76fae6528dd919a423eec
Alpesh-Jadav <alpeshjadav11899@gmail.com>
2020-08-25 00:09:59 -0700

    completed spotify footer

--- src/Body.js
+++ src/Body.js
@@ -3,7 +3,7 @@ import './Body.css'
 function Body() {
     return (
         <div className="body">
-            <h1>body</h1>
+            <h1>Player body</h1>
         </div>
     )
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,51 @@
 import React from 'react'
 import './Footer.css'
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import SpeakerIcon from '@material-ui/icons/Speaker';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import { Grid, Slider } from '@material-ui/core';
 function Footer() {
     return (
         <div className="footer">
             <div className="footer__left">
-                <p>Album and song details</p>
+            <img className="footer__albumLogo" src="https://upload.wikimedia.org/wikipedia/en/d/dc/Kabir_Singh.jpg" alt="poster"/>
+                <div className="footer__songInfo">
+                    <h4>Kaise hua</h4>
+                    <p>Vishal Mishra</p>
+                </div>
             </div>
             <div className="footer__center">
-                <p>Player controls</p>
+            <ShuffleIcon className="footer__icon" />
+            <SkipPreviousIcon className="footer__icon" />
+            <PlayCircleOutlineIcon fontSize="large" className="footer__icon play-btn" />
+            <SkipNextIcon className="footer__icon" />
+            <RepeatIcon className="footer__icon"  />
             </div>
 
             <div className="footer__right">
-                <p>volume controls</p>
+                <Grid container spacing={2}>
+                <Grid item>
+                    <PlaylistPlayIcon className="footer__icon" />
+                </Grid>
+                <Grid item>
+                    <SpeakerIcon className="footer__icon" />
+                </Grid>
+                <Grid item>
+                    <VolumeDownIcon className="footer__icon" />
+                </Grid>
+                <Grid item xs>
+                    <Slider />
+                </Grid>
+                </Grid>
             </div>
         </div>
     )
 }
+
 
 export default Footer
