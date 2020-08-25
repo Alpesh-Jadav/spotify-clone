d478e2a95e35f35596cf03a7bcedf9a7b4ea6277
Alpesh-Jadav <alpeshjadav11899@gmail.com>
2020-08-24 20:34:56 -0700

    important comment NOTICE

--- src/reducer.js
+++ src/reducer.js
@@ -5,6 +5,8 @@ export const initialState = {
     playing: false,
     item: null,
     token: "BQBxlzvhoxjsAhl4obV9mFFNwXUzY0USQmaEXkx7FcbDJJjxnmR6O3A578FkjwzLaTvVkVRQafbdywkAamai9sDHkHvRM6ZYFRxNZ7Pbz85Wr9OV0PHWfGExx3oX8nP2p9kk8RlNbNXQfi-UbXpDV9zlF9T8xNw"
+    // IMP NOTICE: After production of this project , set the above token value to null. 
+    // So every user first go through spotify user autehentication
 };
 
  const reducer = (state, action) => {
