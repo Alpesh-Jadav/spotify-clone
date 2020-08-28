efeddb670a29276f5ce3b0ace421c401bf9e3eb6
Alpesh-Jadav <alpeshjadav11899@gmail.com>
2020-08-28 11:16:37 +0530

    save

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
@@ -8,8 +8,8 @@ import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 function App() {
-
   const [{user, playlists}, dispatch] = useDataLayerValue();
+
   useEffect(() => {
 
     const hash = getTokenFromUrl();
@@ -55,6 +55,7 @@ function App() {
     
    
     }, []) 
+    // console.log(Object.keys(playlists?.items))
   return (
     <div className="app">
       {
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -9,7 +9,6 @@ import { useDataLayerValue } from './DataLayer';
 function Sidebar() {
 
     const [{ playlists }, dispatch] = useDataLayerValue();
-  
     return (
         <div className="sidebar">
         <img className="sidebar__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify"/>
